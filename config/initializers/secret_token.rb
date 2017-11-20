991c91c7bba28eea04471ec89f5479461085e5b9
Abid Mahmood <abidmahmoodbutt@hotmail.com>
2017-11-20 16:55:25 +0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MobileGhar::Application.config.secret_key_base = 'c1ec724292cbcb9a231292c594db92492b089b1be2731221cedd2ac1134e8f395386af85f0561e2171d7e307da6009f2a7ab4a5a91cee6c018b42a1df5a0c908'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MobileGhar::Application.config.session_store :cookie_store, key: '_mobile_ghar_session'
